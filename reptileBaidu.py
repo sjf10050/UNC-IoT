6d4a30d35d45010315aa700e8119a5afd0ff7943
宋佳峰 <sjf@songjiafengdeMacBook-Pro.local>
2018-11-07 17:36:58 +0800

    add unit test

--- reptileBaidu.py
+++ reptileBaidu.py
@@ -13,7 +13,6 @@ import string
 import config as config
 import dbconfig as dbconfig
 from DBUtils.PooledDB import PooledDB
-
 dbPool = PooledDB(pymysql,5,host=dbconfig.db_Address,user=dbconfig.db_User,passwd=dbconfig.db_Pwd,db=dbconfig.db_name,port=3306)
 
 headers = config.headers
@@ -21,11 +20,15 @@ headers = config.headers
 class reptileBaidu():
     def __init__(self, keyword,dbPool,headers):
         self.keyword=keyword
-        self.tablename=keyword+str(time.strftime('%Y%m%d%H%M%S', time.localtime(time.time())))
+        self.__tablename=str(keyword+str(time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))))
         self.__dbPool = dbPool
         self.__countTag()
         self.__getResultCount()
         self.__headers=headers
+        self.creatTable()
+    def getTableName(self):
+        return self.__tablename
+
 
     def __getResultCount(self):
         url = 'http://www.baidu.com/s?wd=' + urllib.parse.quote(self.keyword)
@@ -42,6 +45,25 @@ class reptileBaidu():
         page = response.read()
         soup = BeautifulSoup(page, 'lxml')
         self.tagCount=len(list(soup.find_all(True)))
+
+    def creatTable(self):
+        sql = "CREATE TABLE `baidu`.%s(`ID` INT NOT NULL AUTO_INCREMENT,`s_title` VARCHAR(45) NULL,`s_link` VARCHAR(120) NULL,PRIMARY KEY (`ID`));" 
+        try:
+            conn = dbPool.connection()
+            cursor=conn.cursor()
+            cursor.execute(sql,(self.__tablename))
+            conn.commit()
+        except Exception as  e:
+            print(e)
+            conn.rollback()
+            return 0
+        finally:
+            cursor.close()
+            conn.close()
+            return 0
+
+
+
     def printtagCount(self):
         print(self.tagCount)
 result_google=reptileBaidu('google',dbPool,config.headers)
--- unitest.py
+++ unitest.py
@@ -2,13 +2,25 @@
 import unittest
 from restfulAPI import *
 from dbAPI import *
-
+import baidu
 class TestdbAPI(unittest.TestCase):
     def test_exportToFile(self):
         self.assertEqual(0,exportToFile('aaa'))
 
     def test_getSearchRecords(self):
-        self.assertIsInstance(getSearchRecords(),str)# detect the return value
+        self.assertIsInstance(getSearchRecords(),str)
+
+    def test_DelResult(self):
+        self.assertEqual(0,DelResult('fnsjkhgnv'))
+        self.assertEqual(1,DelResult(baidu.creatTable('hnbvusbagbngfd')))
+        
+
+# class TestBaidu(unittest.TestCase):
+#     def test_exportToFile(self):
+#         self.assertEqual(0,exportToFile('aaa'))
+
+#     def test_getSearchRecords(self):
+#         self.assertIsInstance(getSearchRecords(),str)# detect the return value
         # with self.assertRaises(ValueError):
         #     exportToFile('*')
 
