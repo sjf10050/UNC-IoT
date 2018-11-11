8291942b3e9d1eaa8b4f0d79a6f2366dbc352cad
宋佳峰 <sjf@songjiafengdeMacBook-Pro.local>
2018-11-11 21:06:19 +0800

    test

--- Demo.py
+++ Demo.py
@@ -1,12 +1,31 @@
+import requests
+
 
 def add(a, b):
     return a+b
 
+
 def minus(a, b):
     return a-b
+
 
 def multi(a, b):
     return a*b
 
+
 def divide(a, b):
     return a/b
+
+sckey='SCU24427Ta543f1d5bec4681a4e437877374ae1605ac9a9f0e79d0'
+
+def get_out_ip():
+    url = r'http://www.trackip.net/'
+    r = requests.get(url)
+    txt = r.text
+    ip = txt[txt.find('title')+6:txt.find('/title')-1]
+    return (ip)
+
+print(get_out_ip())
+
+
+
---
+++ sendMail.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import smtplib
+import requests
+from email.mime.text import MIMEText
+
+def get_out_ip():
+    url = r'http://www.trackip.net/'
+    r = requests.get(url)
+    txt = r.text
+    ip = txt[txt.find('title')+6:txt.find('/title')-1]
+    return (ip)
+
+mailto_list=['573262265@qq.com']           #收件人(列表)
+mail_host="smtp.163.com"            #使用的邮箱的smtp服务器地址，这里是163的smtp地址
+mail_user="18551051565"                           #用户名
+mail_pass="auth163song675"                            #密码
+mail_postfix="163.com"                     #邮箱的后缀，网易就是163.com
+def send_mail(to_list,sub,content):
+    me="IPNotification"+"<"+mail_user+"@"+mail_postfix+">"
+    msg = MIMEText(content,_subtype='plain')
+    msg['Subject'] = sub
+    msg['From'] = me
+    msg['To'] = ";".join(to_list)                #将收件人列表以‘；’分隔
+    try:
+        server = smtplib.SMTP()
+        server.connect(mail_host)                            #连接服务器
+        server.login(mail_user,mail_pass)               #登录操作
+        server.sendmail(me, to_list, msg.as_string())
+        server.close()
+        return True
+    except Exception as e:
+        print(str(e))
+        return False
+for i in range(1):                             #发送1封，上面的列表是几个人，这个就填几
+    if send_mail(mailto_list,"IP changed",get_out_ip()):  #邮件主题和邮件内容
+        #这是最好写点中文，如果随便写，可能会被网易当做垃圾邮件退信
+        print ("done!")
+    else:
+        print ("failed!")
+
